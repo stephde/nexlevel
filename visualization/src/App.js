2ba30f3967f7e70ff6aea534ab1252cf02aa9f5a
Markus Petrykowski <markus.petrykowski@nexenio.com>
2018-02-21 21:11:45 +0100

    add map analisys

---
+++ visualization/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Map from "./components/Map";
+
+class App extends Component {
+  render() {
+    return <Map />;
+  }
+}
+
+export default App;
