6be72d57cfec4b92de8c37fff671c14e547fd72d
Stephan Detje <stephan.detje@student.hpi.de>
2018-02-21 14:31:37 +0100

    add mock route /routing/mock

--- Server/routes/routing.js
+++ Server/routes/routing.js
@@ -12,12 +12,66 @@ router.get('/', function (req, res, next) {
 
     here.getRoute(origin, destination)
         .then(result => {
-            res.sendStatus(200).json({ route: result });
+            res.status(200).json({ route: result }).send()
         })
         .catch(err => {
-            res.sendStatus(500).json({error: err})
+            res.status(500).json({error: err}).send()
         })
-
 });
+
+router.get('/mock', (req, res, next) => {
+
+    const mockRoute = [{
+        isLoading: false,
+        connectionSegments: [
+            {
+                name: "Jacky",
+                type: "BUS",
+                departureTime: {
+                    h: 14,
+                    min: 2
+                },
+                departureName: "U-Bahnhof Bülowstraße",
+                departureLocation: {
+                    latitude: "",
+                    longitute: ""
+                },
+                arrivalTime: {
+                    h: 14,
+                    min: 21
+                },
+                arrivalName: "Alexanderplatz",
+                arrivalLocation: {
+                    latitude: "",
+                    longitute: ""
+                }
+            },
+            {
+                name: "Paul",
+                type: "BUS",
+                departureTime: {
+                    h: 14,
+                    min: 23
+                },
+                departureName: "Alexanderplatz",
+                departureLocation: {
+                    latitude: "",
+                    longitute: ""
+                },
+                arrivalTime: {
+                    h: 14,
+                    min: 35
+                },
+                arrivalName: "Storkower Straße",
+                arrivalLocation: {
+                    latitude: "",
+                    longitute: ""
+                }
+            }
+        ]
+    }]
+
+    res.status(200).json(mockRoute).send()
+})
 
 module.exports = router;
