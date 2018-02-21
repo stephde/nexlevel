0abba99e74660f3ba16ff8d7b52f5fc2be0537b4
Marvin Bornstein <marvinbornstein@gmail.com>
2018-02-21 17:20:56 +0100

    add geolocation

--- Server/components/here.js
+++ Server/components/here.js
@@ -9,7 +9,6 @@ function Here () {
     }
 
     function get(path, params) {
-
         Object.assign(params, apiKey)
 
         return axios.get(url + path, {
@@ -23,6 +22,14 @@ function Here () {
 
         return axios.get(url + path, {
             params: params
+        })
+    }
+
+    function getUrl(url, params) {
+        Object.assign(params, apiKey)
+
+        return axios.get(url, {
+            params: params,
         })
     }
 
@@ -44,10 +51,18 @@ function Here () {
 
     this.autocomplete = function (substr) {
         // http://autocomplete.geocoder.cit.api.here.com/
-        return getCIT("6.2/suggest.json", {
+        return getUrl("http://autocomplete.geocoder.cit.api.here.com/6.2/suggest.json", {
             query: substr,
             prox: '52.4986868,13.3728273',
         }).then(r => r.data).catch(e => console.log(e))
+    }
+
+    this.geocode = locationString => {
+        const url = "https://geocoder.api.here.com/6.2/geocode.json"
+        const params = {
+            searchtext: locationString,
+        }
+        return getUrl(url, params).then(r => r.data.Response).catch(e => console.log(e))
     }
 
     return this
--- Server/tests/here_test.js
+++ Server/tests/here_test.js
@@ -1,12 +1,20 @@
 let Here = require('../components/here')
 let here = new Here()
 
-here.getRoute([52.485617,13.3636133], [52.4986868,13.3728273]).then(r => {
-    console.log(r)
-    console.log(r.route)
-    console.log(r.route[0].publicTransportLine)
-})
+// here.getRoute([52.485617,13.3636133], [52.4986868,13.3728273]).then(r => {
+//     console.log(r)
+//     console.log(r.route)
+//     console.log(r.route[0].publicTransportLine)
+// })
 
 // here.autocomplete('Lebe').then(r => {
 //     console.log(r)
 // })
+
+here.geocode('U Turmstr. [Bus Turmstr.]').then(r => console.log(r))
+
+here.geocode('U Turmstr.').then(r => {
+    console.log('location object:', r.View[0].Result)
+    // r.View[0].Result.filter(r => )
+    // console.log('geocodes:', r.View[0].Result[0].Location.)
+})
