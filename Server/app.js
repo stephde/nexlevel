4b1f9958a846f641afdbe076ff2140b418db7e3e
Markus Petrykowski <markus.petrykowski@nexenio.com>
2018-02-22 10:16:58 +0100

    add cors

--- Server/app.js
+++ Server/app.js
@@ -1,73 +1,83 @@
-let express = require('express');
-let path = require('path');
-let favicon = require('serve-favicon');
-let logger = require('morgan');
-let cookieParser = require('cookie-parser');
-let bodyParser = require('body-parser');
-let mongoose = require('mongoose');
-let passport = require('passport');
+let express = require("express");
+let path = require("path");
+let favicon = require("serve-favicon");
+let logger = require("morgan");
+let cookieParser = require("cookie-parser");
+let bodyParser = require("body-parser");
+let mongoose = require("mongoose");
+let passport = require("passport");
 
-let index = require('./routes/index');
-let routing = require('./routes/routing')
-let users = require('./routes/users');
+let index = require("./routes/index");
+let routing = require("./routes/routing");
+let users = require("./routes/users");
 
-let LocalStrategy = require('passport-local').Strategy;
-let JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const User = require('./models/user');
+let LocalStrategy = require("passport-local").Strategy;
+let JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const User = require("./models/user");
+var cors = require("cors");
 
 let app = express();
+app.use(cors());
 
 // connect to database
-let db = process.env.MONGO_URL || 'localhost/react-native-jwt'
+let db = process.env.MONGO_URL || "localhost/react-native-jwt";
 mongoose.connect(db);
-mongoose.connection.on('error', function() {
-    console.info('Error: Could not connect to MongoDB. Did you forget to run `mongod`?')
+mongoose.connection.on("error", function() {
+  console.info(
+    "Error: Could not connect to MongoDB. Did you forget to run `mongod`?"
+  );
 });
 
 // JWT configuration
- let options = {}
- options.jwtFromRequest = ExtractJwt.fromAuthHeader();
- options.secretOrKey = '7x0jhxt&quot;9(thpX6'
+let options = {};
+options.jwtFromRequest = ExtractJwt.fromAuthHeader();
+options.secretOrKey = "7x0jhxt&quot;9(thpX6";
 
 // Configure Passport to use local strategy for initial authentication.
-passport.use('local', new LocalStrategy(User.authenticate()));
+passport.use("local", new LocalStrategy(User.authenticate()));
 
 // Configure Passport to use JWT strategy to look up Users.
-passport.use('jwt', new JwtStrategy(options, function(jwt_payload, done) {
-  User.findOne({
-    _id: jwt_payload.id
-  }, function(err, user) {
-    if (err) {
-      return done(err, false);
-    }
-    if (user) {
-      done(null, user);
-    } else {
-      done(null, false);
-    }
+passport.use(
+  "jwt",
+  new JwtStrategy(options, function(jwt_payload, done) {
+    User.findOne(
+      {
+        _id: jwt_payload.id
+      },
+      function(err, user) {
+        if (err) {
+          return done(err, false);
+        }
+        if (user) {
+          done(null, user);
+        } else {
+          done(null, false);
+        }
+      }
+    );
   })
-}))
+);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', index);
-app.use('/users', users);
-app.use('/routing', routing)
+app.use("/", index);
+app.use("/users", users);
+app.use("/routing", routing);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  let err = new Error('Not Found');
+  let err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -76,18 +86,18 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render("error", {
     message: err.message,
     error: {}
   });
