357be86b05f4e35e5c7c4a5a47a355869234234f
Marvin Bornstein <marvinbornstein@gmail.com>
2018-02-22 15:24:55 +0100

    improvimprovee

--- Server/scripts/sendRequests.js
+++ Server/scripts/sendRequests.js
@@ -18,6 +18,46 @@ const long = {
 const longBase = 485143;
 const longDiff = 60000;
 
+let MultivariateNormal = require("multivariate-normal").default;
+const meanVector = [[ 52.53444233,  13.50441863],
+    [ 52.47266661,  13.38355267],
+    [ 52.47990096,  13.56537612],
+    [ 52.53525998,  13.32920952],
+    [ 52.52059069,  13.442389  ]]
+
+const covarianceMatrix = [[[ 0.00297764,  0.00057332],
+    [ 0.00057332,  0.00171908]],
+
+    [[ 0.0007809 ,  0.00044683],
+        [ 0.00044683,  0.00117616]],
+
+    [[ 0.00029638,  0.00043767],
+        [ 0.00043767,  0.00127501]],
+
+    [[ 0.00110491,  0.00121153],
+        [ 0.00121153,  0.00355132]],
+
+    [[ 0.00063009, -0.00011077],
+        [-0.00011077,  0.00091973]]];
+
+const weights = [ 0.09415236,  0.21658325,  0.09421823,  0.35455751,  0.24048865]
+const dist = meanVector.map((mean, i) => MultivariateNormal(mean, covarianceMatrix[i]))
+
+
+function genCoord() {
+    let chooseNormal = Math.random()
+    let i = 0
+    weights.forEach((v,j) => {
+        chooseNormal -= v
+        if (chooseNormal < 0) {
+            i = j
+        }
+    })
+    let coords = dist[i].sample()
+    return "[" + coords.toString() + "]";
+}
+
+
 function getNextCoordinate() {
   const latNachkomma = latBase + Math.floor(Math.random() * latDiff);
   const longNachkomma = longBase + Math.floor(Math.random() * longDiff);
@@ -42,16 +82,18 @@ function promisedTimeout(ms) {
 }
 
 async function run() {
-  const numOfRequest = 5;
-  const delayInMs = 100;
+  const numOfRequest = 40;
+  const delayInMs = 600;
 
   let promises = [];
   for (let i = 0; i < numOfRequest; i++) {
     promises.push(
       axios.get(remoteGlitch + path, {
         params: {
+          // origin: genCoord(),
+          // destination: genCoord(),
           origin: getNextCoordinate(),
-          destination: getNextCoordinate()
+          destination: getNextCoordinate(),
         }
       })
     );
